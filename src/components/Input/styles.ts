f9c8c3877afde6755cae42bdc6a290de9267eb80
Augusto <augustotf93@gmail.com>
2020-09-04 13:55:19 -0300

    subindo svg

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -22,7 +22,21 @@ export const Container = styled.div<ContainerProps>`
     flex: 1;
     border: 0;
     background: transparent;
+    width: 100%;
     color: #f4ede8;
+
+    ::-webkit-calendar-picker-indicator {
+      filter: invert(100%);
+    }
+
+    :-webkit-autofill {
+      -webkit-box-shadow: 0 0 0 30px #232129 inset;
+    }
+
+    /* Cor do texto do autocomplete */
+    :-webkit-autofill {
+      -webkit-text-fill-color: white !important;
+    }
 
     ::placeholder {
       color: #666360;
