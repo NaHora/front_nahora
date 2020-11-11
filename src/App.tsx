1282f35360d481835092458896b7ba3d614296fb
toguvr <augustotf93@gmail.com>
2020-11-11 13:08:06 -0300

    subindo app com refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import numeral from 'numeral';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import AppProvider from './hooks';
 import 'numeral/locales/pt-br';
+import * as serviceWorker from './serviceWorker';
+import { Snackbar, Button } from '@material-ui/core';
 
 const App: React.FC = () => {
   numeral.locale('pt-br');
+  const [showReload, setShowReload] = useState(false);
+  const [waitingWorker, setWaitingWorker] = useState<ServiceWorker | null>(
+    null,
+  );
+
+  const onSWUpdate = (registration: ServiceWorkerRegistration) => {
+    setShowReload(true);
+    setWaitingWorker(registration.waiting);
+  };
+
+  useEffect(() => {
+    serviceWorker.register({ onUpdate: onSWUpdate });
+  }, []);
+
+  const reloadPage = () => {
+    waitingWorker?.postMessage({ type: 'SKIP_WAITING' });
+    setShowReload(false);
+    window.location.reload(true);
+  };
+
   return (
     <Router>
+      <Snackbar
+        open={showReload}
+        message="Nova versão disponível!"
+        onClick={reloadPage}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        action={
+          <Button color="inherit" size="small" onClick={reloadPage}>
+            Atualizar
+          </Button>
+        }
+      />
       <AppProvider>
         <Routes />
       </AppProvider>
